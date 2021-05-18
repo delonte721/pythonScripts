a02cbd2c4d4c4a9b659519c3f8a03e78a9e927e1
Delonte Stewart <lonte721@gmail.com>
2021-05-18 07:06:13 -0400

    commit

--- firstscript.py
+++ firstscript.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-#This program says hello and asks for my name 
+#This program says hello and asks for user name 
 
 print('Hello World!')
 print('What is your name?') #ask for thier name 
-myName= raw_input()
+myName= input()
 print('It is good to meet you, ' + myName)
 print('The length of your name is:')
 print(len(myName))
 
 print('What is your age?') #ask for thier age 
-myAge= raw_input()
-print('You will be ' + str(int(myAge) + 1) + ' in the year 2020. Happy New Year!!!!')
+myAge= input()
+print('You will be ' + str(int(myAge) + 1) + ' in the year 2021. Happy New Year!!!!')
