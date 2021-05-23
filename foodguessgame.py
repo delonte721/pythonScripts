eb7d18eacfffbf9910960f49c72cfa37f9870109
Delonte Stewart <lonte721@gmail.com>
2021-05-23 17:36:11 -0400

    commit

---
+++ foodguessgame.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+print("Hello, what is your name")
+name = input()
+
+print("Well, "+name+", What's Delonte's favorite food?")
+myfavoritefood = "tacos"
+
+if input() == myfavoritefood:
+    print("Yep!, So amazing!")
+else:
+    print("Yuck, that's not it!")
+    print("Thanks, for playing!")
